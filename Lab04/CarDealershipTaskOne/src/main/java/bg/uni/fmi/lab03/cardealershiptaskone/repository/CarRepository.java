e427b8aad3b967559d1221f6188fb4c82102db81
Stefan Velev <sdvelev@gmail.com>
2023-03-29 16:57:23 +0300

    Revert "Revert "Add solution to Task 1 in Lab04""
    This reverts commit 0bfd872416b18b1a651fb270498d99ad51ee0280.

---
+++ Lab04/CarDealershipTaskOne/src/main/java/bg/uni/fmi/lab03/cardealershiptaskone/repository/CarRepository.java
@@ -0,0 +1,44 @@
+package bg.uni.fmi.lab03.cardealershiptaskone.repository;
+
+import bg.uni.fmi.lab03.cardealershiptaskone.model.Car;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class CarRepository {
+    private final List<Car> carInventory;
+
+    public CarRepository() {
+        this.carInventory = new ArrayList<>();
+    }
+
+    public CarRepository(List<Car> carInventory) {
+        this.carInventory = carInventory;
+    }
+
+    public void addCar(Car carToAdd) {
+        this.carInventory.add(carToAdd);
+    }
+
+    public void removeCar(Car carToRemove) {
+        this.carInventory.remove(carToRemove);
+    }
+
+    public List<Car> searchCars(String make, String model, int year, double price) {
+
+        return this.carInventory.stream()
+            .filter(car -> car.getMake().equals(make) &&
+                car.getModel().equals(model) &&
+                car.getYear() == year &&
+                car.getPrice() == price)
+            .collect(Collectors.toList());
+    }
+
+    public List<Car> getAllCars() {
+
+        return this.carInventory;
+    }
+}
