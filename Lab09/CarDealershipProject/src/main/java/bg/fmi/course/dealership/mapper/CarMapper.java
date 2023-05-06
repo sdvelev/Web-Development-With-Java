eb4985f26c510060f7b66980fbb4e7f8006b25f0
Stefan Velev <sdvelev@gmail.com>
2023-05-06 23:46:25 +0300

    Add CarMapper.java

---
+++ Lab09/CarDealershipProject/src/main/java/bg/fmi/course/dealership/mapper/CarMapper.java
@@ -0,0 +1,40 @@
+package bg.fmi.course.dealership.mapper;
+
+import bg.fmi.course.dealership.dto.CarDto;
+import bg.fmi.course.dealership.dto.DealershipDto;
+import bg.fmi.course.dealership.model.Car;
+import bg.fmi.course.dealership.model.Dealership;
+import bg.fmi.course.dealership.model.Sale;
+import bg.fmi.course.dealership.model.SalesPerson;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CarMapper {
+
+    public CarDto toDto(Car entity) {
+        return CarDto.builder()
+            .id(entity.getId())
+            .dealershipId(entity.getDealership().getId())
+            .saleId(entity.getSale().getId())
+            .make(entity.getMake())
+            .model(entity.getModel())
+            .year(entity.getYear())
+            .color(entity.getColor())
+            .price(entity.getPrice())
+            .build();
+    }
+
+    public Car toEntity(CarDto carDto, Dealership dealership, Sale sale) {
+        return Car.builder()
+            .id(carDto.getId())
+            .dealership(dealership)
+            .sale(sale)
+            .color(carDto.getColor())
+            .make(carDto.getMake())
+            .model(carDto.getModel())
+            .price(carDto.getPrice())
+            .year(carDto.getYear())
+            .build();
+    }
+
+}
